758fa8cd5559bc77a7b643fb7c0867fc14be969a
magicmon <sagun25si@gmail.com>
2017-03-28 16:24:59 +0900

    bug fix.

--- Mentions/Classes/UITextInput/MentionTextView.swift
+++ Mentions/Classes/UITextInput/MentionTextView.swift
@@ -40,7 +40,7 @@ public class MentionTextView: UITextView {
                     highlightUsers.append(user, range)
                 }
                 
-                refreshAttributedText()
+                refresh()
             }
         }
     }
